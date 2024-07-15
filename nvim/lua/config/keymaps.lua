e934d428f1fd246341144d1f04d44fe04fc398df
Gian Sanchez <giansanchez71@gmail.com>
2024-07-15 10:37:12 +0800

    nvim: add harpoon

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,3 +9,12 @@ map("n", "<S-Enter>", "O<ESC>", { desc = "Add space before" })
 map("n", "<Enter>", "o<ESC>", { desc = "Add space after" })
 
 map("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
+
+map("n", "<C-d>", "<C-d>zz", { silent = true })
+map("n", "<C-u>", "<C-u>zz", { silent = true })
+map("n", "n", "nzzzv", { silent = true })
+map("n", "N", "Nzzzv", { silent = true })
+map("n", "H", "zH", { silent = true })
+map("v", "H", "zH", { silent = true })
+map("n", "L", "zL", { silent = true })
+map("v", "L", "zL", { silent = true })
